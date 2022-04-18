ae210ef4602e7b2848e78d1d4d8b5ec83ee33035
Colten Empey <colten@empey.me>
2022-04-18 15:05:20 -0600

    all finished up!

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
 export const ADD_ONE = "ADD_ONE";
-
+export const CLEAR = "CLEAR"
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MEMORY_APPLY = "MEMORY_APPLY";
+export const MEMORY_CLEAR = "MEMORY_CLEAR"
 
 export const addOne = () => {
     return({type:ADD_ONE});
@@ -9,4 +12,22 @@ export const addOne = () => {
 
 export const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
+}
+
+export const changeOperation = (operator) =>{
+    return ({type: CHANGE_OPERATION, payload: operator})
+}
+export const clear = ()=>{
+    return ({type: CLEAR})
+}
+
+export const memoryAdd = () =>{
+    return ({type: MEMORY_ADD})
+}
+
+export const memoryApply = () =>{
+    return ({type: MEMORY_APPLY})
+}
+export const momoryClear = () =>{
+    return ({type: MEMORY_CLEAR})
 }
